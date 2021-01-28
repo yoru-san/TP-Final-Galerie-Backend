69e6ddb74352bb9d1de1e36e2c4f8901e954f4b8
yoru-san <cecel31@free.fr>
2021-01-28 17:49:38 +0100

    Added PWA icons

--- Galerie-PWA/sw.js
+++ Galerie-PWA/sw.js
@@ -9,7 +9,7 @@ importScripts("./service_worker/sync.js");
 const cacheName = "galerie-v1";
 
 const files = [
-  "/",
-  "/script.js",  
+  "./",
+  "./script.js",  
   "https://cdnjs.cloudflare.com/ajax/libs/localforage/1.7.3/localforage.min.js"
 ];
